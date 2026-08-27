5cf6c530bdede52161200ee5e8b24275015ee41c
KnTrickster <kevin.torquato.gamer@gmail.com>
2026-08-27 05:39:09 -0300

    Anemoia V1

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function triggerGlitch() {
         const axis = Math.random() < 0.5 ? "x" : "y";
         let displacementX = 0;
         let displacementY = 0;
-        
+
         bar.style.transform = "translate(0, 0)";
 
         setTimeout(() => {
@@ -55,6 +55,165 @@ function triggerGlitch() {
     const nextGlitch = Math.random() * 7000 + 3000;
     setTimeout(triggerGlitch, nextGlitch);
 }
-
 triggerGlitch();
-console.log("JS tá on!")
+
+const skills = {
+    programming: [
+        "Java",
+        "Python",
+        "JavaScript",
+        "HTML / CSS"
+    ],
+
+    database: [
+        "SQL",
+        "Data Modeling"
+    ],
+
+    technologies: [
+        "Spring Boot",
+        "REST APIs",
+        "Git / GitHub"
+    ],
+
+    security: [
+        "Cybersecurity Fundamentals",
+        "Networking Fundamentals",
+        "Linux"
+    ],
+
+    languages: [
+        "Portuguese - Native",
+        "English - B2 (upper-intermediate stage, by TOEIC)",
+        "Spanish - Basic"
+    ],
+
+    soft: [
+        "Problem Solving",
+        "Logical Thinking",
+        "Communication",
+        "Teamwork",
+        "Continuous Learning"
+    ]
+}
+function renderSkills() {
+    const skillCategories = {
+        programming: document.getElementById("programming-skills"),
+        database: document.getElementById("database-skills"),
+        technologies: document.getElementById("technology-skills"),
+        security: document.getElementById("security-skills"),
+        languages: document.getElementById("language-skills"),
+        soft: document.getElementById("soft-skills")
+    };
+    for (const category in skills) {
+        const container = skillCategories[category];
+
+        skills[category].forEach(skill => {
+            const skillItem = document.createElement("div");
+            skillItem.classList.add("skill-item");
+            skillItem.textContent = skill;
+            container.appendChild(skillItem);
+        });
+    }
+}
+renderSkills();
+
+const projects = [
+    {
+        name: "ANEMOIA",
+        description: "A retro-futuristic personal website built with HTML, CSS and JavaScript, featuring custom CRT effects, glitches, interactive elements and easter eggs.",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        type: "PERSONAL PROJECT",
+        icon: "assets/images/streamline-pixel---free--32x32-SVG/site-icon.svg",
+        github: "https://github.com/KnTrickster/Anemoia_Personal_System"
+    },
+
+    {
+        name: "CAMPEONATO MUNDIAL",
+        description: "Database modeling project for a fictional league of unconventional sports. Made with friends!",
+        technologies: ["SQL", "Oracle", "Data Modeling"],
+        type: "ACADEMIC PROJECT",
+        icon: "assets/images/streamline-pixel---free--32x32-SVG/database-icon.svg",
+        github: "https://github.com/KnTrickster/Campeonato-Mundial-de-Esportes-Impossiveis"
+    }
+]
+
+function renderProjects() {
+    const container = document.getElementById("projects-container");
+
+    projects.forEach(project => {
+        const card = document.createElement("article");
+        card.classList.add("project-card");
+
+        card.innerHTML = `
+            <div class= "project-icon">
+                <img src= "${project.icon}" alt="">
+            </div>
+
+            <div class= "project-content">
+                <span class= "project-type">
+                    ${project.type}
+                </span>
+            
+
+                <h3>${project.name}</h3>
+                <p>${project.description}</p>
+
+                <div class= "project-technologies">
+                    ${project.technologies
+                    .map(technology => `<span>${technology}</span>`)
+                    .join("")}
+                </div>
+            
+                <a href ="${project.github}" target="_blank">
+                        VIEW ON GITHUB!
+                </a>
+            </div>
+        `;
+        container.appendChild(card);
+    });
+}
+renderProjects();
+
+const contacts = [
+    {
+        channel: "EMAIL",
+        handle: "ktorquato.ti@gmail.com",
+        status: "[ACTIVE]",
+        link: "mailto:ktorquato.ti@gmail.com"
+    },
+    {
+        channel: "GITHUB",
+        handle: "@kntrickster",
+        status: "[ACTIVE]",
+        link: "https://github.com/KnTrickster"
+    },
+    {
+        channel: "LINKEDIN",
+        handle: "linkedin.com/in/kevin-torquato",
+        status: "[ACTIVE]",
+        link: "https://www.linkedin.com/in/kevin-torquato-1830a52b7/"
+    }
+]
+
+function renderContacts() {
+    const contactList = document.getElementById("contact-list");
+
+    contactList.innerHTML = "";
+
+    contacts.forEach(contact => {
+        const row = document.createElement("div");
+        row.classList.add("contact-row");
+
+        row.innerHTML = `
+            <span>${contact.channel}</span>
+            <a href="${contact.link}" target="_blank" rel="noopener noreferrer">
+                ${contact.handle}
+            </a>
+            <span>${contact.status}</span>
+        `;
+
+        contactList.appendChild(row);
+    });
+}
+renderContacts();
